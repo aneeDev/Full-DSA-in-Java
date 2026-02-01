6c5bfd826b516780a29443c01b152002d4da6590
Aneervan Ray <aneervan.raja2000@gmail.com>
2026-02-01 12:31:03 +0530

    Add Selection Sort implementation and update README with sorting techniques

---
+++ 4-Important-Sorting-Techniques/README.md
@@ -0,0 +1,20 @@
+# Important Sorting Techniques
+
+This directory contains implementations of various important sorting algorithms.
+
+<table>
+    <tr style="background-color:#0052CC; color:white;">
+        <th>Sl No.</th>
+        <th>Problem</th>
+        <th>Level</th>
+        <th>GFG</th>
+        <th>Leetcode</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Selection Sort</td>
+        <td style="background-color:#198754; color:white;">Easy</td>
+        <td><a href="https://www.geeksforgeeks.org/problems/selection-sort/1">Selection Sort</a></td>
+        <td>NA</td>
+    </tr>
+</table>
---
+++ 4-Important-Sorting-Techniques/src/main/java/selectionSort.java
@@ -0,0 +1,73 @@
+package main.java;
+
+/**
+ * <h4>Selection Sort</h4>
+ * <hr>
+ * Sorts an array using the Selection Sort algorithm.
+ *
+ * <p>Selection Sort repeatedly selects the minimum element from the unsorted part and puts it at the beginning.
+ *
+ * <p>Example:
+ * <ul>
+ *   <li>Input: arr[] = [4, 1, 3, 9, 7]</li>
+ *   <li>Output: [1, 3, 4, 7, 9]</li>
+ *   <li>Input: arr[] = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]</li>
+ *   <li>Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]</li>
+ *   <li>Input: arr[] = [38, 31, 20, 14, 30]</li>
+ *   <li>Output: [14, 20, 30, 31, 38]</li>
+ * </ul>
+ * <pre>
+ * int[] arr = {4, 1, 3, 9, 7};
+ * selectionSort.sort(arr);
+ * // arr is now [1, 3, 4, 7, 9]
+ * </pre>
+ *
+ * <p>Constraints:
+ * <ul>
+ *   <li>1 ≤ arr.length ≤ 10^3</li>
+ *   <li>1 ≤ arr[i] ≤ 10^6</li>
+ * </ul>
+ *
+ * @see <a href="https://www.geeksforgeeks.org/problems/selection-sort/1">GFG: Selection Sort (E)</a>
+ */
+public class selectionSort {
+
+    /**
+     * Sorts the given array using Selection Sort algorithm.
+     * <p>
+     *     <b>Complexirt Analysis</b>
+     *     <ul>
+     *         <li>Time Complexity: O(n^2) in all cases (Best, Average, Worst)</li>
+     *         <li>Space Complexity: O(1)</li>
+     *     </ul>
+     * </p>
+     * @param arr
+     */
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        // Traverse through all array elements
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int minIdx = i;
+            for (int j = i; j <= n - 1; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+                int temp = arr[minIdx];
+                arr[minIdx] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+
+    // Driver method to test above
+    public static void main(String[] args) {
+        int[] arr = {10,9,8,7,6,5,4,3,2,1};
+        selectionSort(arr);
+        System.out.println("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
